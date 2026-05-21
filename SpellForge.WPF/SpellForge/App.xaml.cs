afee6360e9b1153009da4572b90b5d91c3884647
Anakin Raymer <anakin.raymer@gmail.com>
2026-05-21 11:29:40 -0600

    Fix startup crash: ProgressBar.LevelProgress TwoWay binding; add unhandled exception handler

--- SpellForge.WPF/SpellForge/App.xaml.cs
+++ SpellForge.WPF/SpellForge/App.xaml.cs
@@ -1,7 +1,20 @@
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SpellForge;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        base.OnStartup(e);
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(e.Exception.Message, "SpellForge Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
+    }
 }
